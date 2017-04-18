5e95bab741a3a4d9abb2efa5dba7f11f1e83bc0a
Stefan Holst <mail@s-holst.de>
2017-04-18 15:35:27 +0900

    preparation for exporting multi-clock designs.

--- src/jp/ac/kyutech/ci/grouping/Main.java
+++ src/jp/ac/kyutech/ci/grouping/Main.java
@@ -1,57 +1,73 @@
 package jp.ac.kyutech.ci.grouping;
 
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
 import org.junit.Test;
+import org.kyupi.graph.FormatVerilog;
 import org.kyupi.graph.Graph;
+import org.kyupi.graph.Graph.Node;
+import org.kyupi.graph.LibraryOldSAED;
 import org.kyupi.graph.LibrarySAED;
 import org.kyupi.graph.ScanChains;
-import org.kyupi.graph.Graph.Node;
 import org.kyupi.graph.ScanChains.ScanCell;
 import org.kyupi.graph.ScanChains.ScanChain;
 import org.kyupi.misc.KyupiApp;
 
 public class Main extends KyupiApp {
 
-	
 	public static void main(String[] args) throws Exception {
 		new Main().setArgs(args).call();
 	}
 
 	public Main() {
 		options.addOption("def", true, "input def file");
+		options.addOption("separate_clocks", true,
+				"safe a new design with separate clock for each chain to given file.");
 	}
-	
+
 	private Graph circuit;
-	
+
 	@Override
 	public Void call() throws Exception {
-		
+
 		printWelcome();
 
-		setLib(new LibrarySAED());
-		
+		setLib(new LibraryOldSAED());
+
 		circuit = loadCircuitFromArgs();
-		
+
 		circuit.printStats();
-		
+
 		ScanChains chains = new ScanChains(circuit);
-		
+
 		log.info("ScanChainCount " + chains.size());
-		
+
 		for (int chainIdx = 0; chainIdx < chains.size(); chainIdx++) {
 			ScanChain chain = chains.get(chainIdx);
 			log.info("Chain " + chainIdx + " ScanInPort " + chain.in.node.queryName());
-			for (ScanCell cell: chain.cells) {
-				log.info("  ScanCell " + cell.node.queryName());				
+			for (ScanCell cell : chain.cells) {
+				log.info("  ScanCell " + cell.node.queryName());
 			}
 		}
-		
-		collectClockBuffers(circuit, chains);
-		
+
+		CBInfo cbinfo = collectClockBuffers(circuit, chains);
+
+		if (argsParsed().hasOption("separate_clocks")) {
+			String filename = argsParsed().getOptionValue("separate_clocks");
+			separateClocks(chains, cbinfo);
+			FileOutputStream os = new FileOutputStream(filename);
+			FormatVerilog.save(os, circuit, "foo");
+			os.close();
+		}
+
 		return null;
+	}
+
+	private void separateClocks(ScanChains chains, CBInfo cbinfo) {
+		// TODO Auto-generated method stub
 	}
 
 	@Test
@@ -62,12 +78,12 @@ public class Main extends KyupiApp {
 		assertEquals(45, circuit.countInputs());
 		assertEquals(32, circuit.countOutputs());
 	}
-	
+
 	class CBInfo {
 		public HashSet<Node> all_clock_buffers = new HashSet<>();
 		public HashMap<Node, HashSet<Node>> sff_to_clock_buffer_set = new HashMap<>();
 	}
-	
+
 	private CBInfo collectClockBuffers(Graph graph, ScanChains ci) {
 		log.info("Collecting clock buffers for each scan cell");
 
@@ -86,10 +102,10 @@ public class Main extends KyupiApp {
 				for (Node n : cb) {
 					sbuf.append(" " + n.queryName());
 				}
-				log.debug("buffer " + sbuf);
+				// log.debug("buffer " + sbuf);
 				sff_to_cb.put(sffnode, cb);
 				all_cb.addAll(cb);
-				//log.debug(sffnode + " " + clock_buffers.get(sffnode));
+				// log.debug(sffnode + " " + clock_buffers.get(sffnode));
 			}
 		}
 
@@ -124,7 +140,7 @@ public class Main extends KyupiApp {
 
 		return cbi;
 	}
-	
+
 	private ArrayList<Node> collectClockBuffers(Node head, ArrayList<Node> tail) {
 		if (head == null)
 			return tail;
