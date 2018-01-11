8ea948aee0ea2dac4e040da288620a30b147028f
blankAvailable <zhang-y-c@outlook.com>
2018-01-11 14:54:32 +0900

    make z2 available when k >= chains.size()

--- src/jp/ac/kyutech/ci/grouping/ScanChainGrouperAlgZ2.java
+++ src/jp/ac/kyutech/ci/grouping/ScanChainGrouperAlgZ2.java
@@ -26,57 +26,61 @@ public class ScanChainGrouperAlgZ2 extends ScanChainGrouper {
         threshold = cost.evaluate(thrClocking, chains.size());
         log.info("Lower bound: " + threshold);
 
-        Random r = new Random();
-        int[] clocksFlag = new int[clockCount];
+        if (clockCount >= chains.size()){
+            return thrClocking;
+        }else {
+            Random r = new Random();
+            int[] clocksFlag = new int[clockCount];
 
-        int[] clocking = new int[chains.size()];
-        for (int i = 0; i < chains.size(); i++)
-            clocking[i] = -1;
-        clocking[0] = r.nextInt(clockCount);
-        clocksFlag[clocking[0]] = 1;
+            int[] clocking = new int[chains.size()];
+            for (int i = 0; i < chains.size(); i++)
+                clocking[i] = -1;
+            clocking[0] = r.nextInt(clockCount);
+            clocksFlag[clocking[0]] = 1;
 
-        for (int chainIdx = 1; chainIdx < chains.size(); chainIdx++){
+            for (int chainIdx = 1; chainIdx < chains.size(); chainIdx++) {
 
-            System.out.println("Clocking " + Arrays.toString(clocking).replaceAll("\\[", "").replaceAll("\\]", "")
-                    .replaceAll(",", ""));
+                System.out.println("Clocking " + Arrays.toString(clocking).replaceAll("\\[", "").replaceAll("\\]", "")
+                        .replaceAll(",", ""));
 
-            if (availableGroupExist(clocksFlag)){
-                for (int clkIdx = 0; clkIdx < clockCount; clkIdx++){
-                    if (clocksFlag[clkIdx] == 1){
-                        clocking[chainIdx] = clkIdx;
-                        if (cost.evaluate_usable(clocking, clockCount, threshold)) {
-                            log.info("group " + clkIdx + " is reusable");
-                            break;
+                if (availableGroupExist(clocksFlag)) {
+                    for (int clkIdx = 0; clkIdx < clockCount; clkIdx++) {
+                        if (clocksFlag[clkIdx] == 1) {
+                            clocking[chainIdx] = clkIdx;
+                            if (cost.evaluate_usable(clocking, clockCount, threshold)) {
+                                log.info("group " + clkIdx + " is reusable");
+                                break;
+                            }
+                            clocking[chainIdx] = -1;
                         }
-                        clocking[chainIdx] = -1;
                     }
-                }
-                if (clocking[chainIdx] == -1){
-                    int i;
-                    do{
-                        i = r.nextInt(clockCount);
-                        clocking[chainIdx] = i;
-                    }while(clocksFlag[i] == 1);
-                    log.info("randomly assign a available group " + i);
-                    clocksFlag[i] = 1;
-                }
-            }else {
-                int tempMinCost = Integer.MAX_VALUE;
-                int tempBestGroup = -1;
-                for (int clkIdx = 0; clkIdx < clockCount; clkIdx++){
-                    clocking[chainIdx] = clkIdx;
-                    if (tempMinCost > cost.evaluate(clocking, clockCount)){
-                        tempMinCost = cost.evaluate(clocking, clockCount);
-                        tempBestGroup = clkIdx;
+                    if (clocking[chainIdx] == -1) {
+                        int i;
+                        do {
+                            i = r.nextInt(clockCount);
+                            clocking[chainIdx] = i;
+                        } while (clocksFlag[i] == 1);
+                        log.info("randomly assign a available group " + i);
+                        clocksFlag[i] = 1;
                     }
+                } else {
+                    int tempMinCost = Integer.MAX_VALUE;
+                    int tempBestGroup = -1;
+                    for (int clkIdx = 0; clkIdx < clockCount; clkIdx++) {
+                        clocking[chainIdx] = clkIdx;
+                        if (tempMinCost > cost.evaluate(clocking, clockCount)) {
+                            tempMinCost = cost.evaluate(clocking, clockCount);
+                            tempBestGroup = clkIdx;
+                        }
+                    }
+                    log.info("The most suitable group is " + tempBestGroup);
+                    clocking[chainIdx] = tempBestGroup;
+                    clocksFlag[tempBestGroup] = 1;
                 }
-                log.info("The most suitable group is " + tempBestGroup);
-                clocking[chainIdx] = tempBestGroup;
-                clocksFlag[tempBestGroup] = 1;
             }
-        }
 
-        return clocking;
+            return clocking;
+        }
     }
 
     private boolean availableGroupExist(int[] clocksFlag){
